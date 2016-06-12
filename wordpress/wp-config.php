2835eab726c0f5f42fc694b8df0321ef8970141b
timo77 <timo77@antioch.jp>
2016-06-12 19:51:20 +0900

    2

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -52,13 +52,13 @@
 
     // MULTISITE
     define ('WP_ALLOW_MULTISITE', true);
-    define('MULTISITE', true);
+/*    define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', true);
 define('DOMAIN_CURRENT_SITE', 'tv.antioch.jp');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
-
+*/
     /**#@+
      * Authentication Unique Keys and Salts.
      *
